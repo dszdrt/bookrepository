9cbf033c46c7c2f2cfb063cdb4ad69de201d32a4
dszdrt <mizmixy@gmail.com>
2025-03-08 16:57:57 +0300

    Update BookRepository.java

--- book/src/main/java/bookrepository/repository/BookRepository.java
+++ book/src/main/java/bookrepository/repository/BookRepository.java
@@ -5,9 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-<<<<<<< HEAD
+}
 
-}
-=======
-}
->>>>>>> cd6bc207bf29e2b6d94527a8ca99de4e2ae256a2
+
+
